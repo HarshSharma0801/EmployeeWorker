3f2bd5483ce62897ee65325dd1080129377673dd
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-02-14 23:46:52 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee tim = new Employee("Tim", "11/11/1985",
+                "01/01/2020");
+        System.out.println(tim);
+        System.out.println("Age = " + tim.getAge());
+        System.out.println("Pay = " + tim.collectPay());
+
+
+        SalariedEmployee joe = new SalariedEmployee("Joe", "11/11/1990",
+                "03/03/2020",35000);
+        System.out.println(joe);
+        System.out.println("joe's paycheck is $"+ joe.collectPay());
+        joe.retire();
+        System.out.println("joe's pension pay is $"+joe.collectPay());
+
+    }
+}
